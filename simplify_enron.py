106fef5d8b71a9b698ab8570692e00cea4cd9507
Eric Campbell <1234goal@gmail.com>
2016-08-10 16:44:15 -0700

    Add testers

--- simplify_enron.py
+++ simplify_enron.py
@@ -1,15 +1,15 @@
-print('hello world')
-print("HELLO WORLD")
-
 import mysql.connector
 from getpass import getpass
+
+table_name = "edges_twenty"
+num_days   = "20"
 
 
 def main():
     print("executing main")
     pw = getpass()
     db = mysql.connector.connect(user="root",
-                                 db = "enronr",
+                                 db = "enron",
                                  passwd = pw)
 
     print("connecting to database, preparing data")
@@ -21,46 +21,48 @@ def main():
         print("failed")
 
 def simplify(db):
+    global table_name
     c = db.cursor(buffered=True)
+    print("loading edge_point")
     c.execute("""SELECT * FROM edge_point ORDER BY source_id, dest_id, time""")
     edges = c.fetchall()
-    
+    print("writing to", table_name)    
     for p_edge in edges:
-        print("POINT EDGE", p_edge)
+        # print("POINT EDGE", p_edge)
         
         cond_str = """((E.dest_id = {1} AND E.source_id = {2})
                          OR (E.source_id = {1} AND E.dest_id = {2}))
-                      AND E.start - 86400 <= {3} AND E.end + 86400 >= {3}
-                   """.format(*p_edge)
+                      AND E.start - {4}*86400 <= {3} AND E.end + {4}*86400 >= {3}
+                   """.format(*p_edge, num_days)
 
-        mtchstr = """SELECT * FROM edges E WHERE {0}""".format(cond_str)
+        mtchstr = """SELECT * FROM {1} E WHERE {0}""".format(cond_str, table_name)
 
-        print(mtchstr)
+        # print(mtchstr)
         c.execute(mtchstr)
 
         matched = c.fetchall()
         
-        print("MATCHED EDGES", matched)
+        # print("MATCHED EDGES", matched)
         
         if matched:
             for e in matched:
-                print("Updating with",p_edge, "and", e)
+                # print("\tUpdating with", p_edge, "and", e)
                 c.execute("""
-                          UPDATE edges E
+                          UPDATE {5} E
                           SET E.start = LEAST   (E.start, {3}),
                               E.end   = GREATEST(E.end  , {3})
-                          WHERE E.edge_id = {4}
-                          """.format(*p_edge, e[0]))
+                          WHERE E.edge_id = {4} AND ({3} < E.start OR E.end < {3})
+                          """.format(*p_edge, e[0], table_name))
         else:
-            print("inserting", p_edge)
+            # print("\tinserting", p_edge, "into", table_name)
             c.execute("""
-                      INSERT INTO edges (edge_id,source_id, dest_id, start, end)
+                      INSERT INTO {4} (edge_id,source_id, dest_id, start, end)
                                  VALUES (    {0},      {1},     {2},   {3}, {3})
-                      """.format(*p_edge))
+                      """.format(*p_edge, table_name))
 
         
-        print()
-        print()
+        # print()
+        # print()
         db.commit()
 
     c.close()
@@ -70,8 +72,20 @@ def simplify(db):
 
 
 def prep(db):
+    global table_name
     c = db.cursor()
-    c.execute("""DELETE FROM edges""")
+    create = """CREATE TABLE IF NOT EXISTS
+                              {0} (edge_id INT PRIMARY KEY, 
+                                    source_id INT, 
+                                    dest_id INT, 
+                                    start INT, 
+                                    end INT)
+              """.format(table_name)
+    empty = """DELETE FROM {0}""".format(table_name)
+    source_dest_idx = """ALTER TABLE {0} ADD INDEX (source_id, dest_id)""".format(table_name)
+    start_end_idx = """ALTER TABLE {0} ADD INDEX (source_id, dest_id)""".format(table_name)
+    c.execute(create)
+    c.execute(empty)
     c.close()
     db.commit()
 
--- test_subgraphs.py
+++ test_subgraphs.py
@@ -1,8 +1,10 @@
 import argparse
 import random
+from math import inf
 
 def main():
     parser = argparse.ArgumentParser(description="generate query graphs")
+    parser.add_argument("implicit")
     parser.add_argument("structure")
     parser.add_argument("-v","--num-vertices", type=int)
     parser.add_argument("-e","--num-edges", type=int)
@@ -10,36 +12,49 @@ def main():
     parser.add_argument("-t","--max-time", type=int, help="maximum end time")
     parser.add_argument("-n","--num-graphs", type=int)
 
+
     args = parser.parse_args()
+
+    if args.implicit not in ("CONCUR", "SCONSEC", "WCONSEC"):
+        print("Unrecognized implicit semantics", implicit)
+        print("\tPlease choose from CONCUR, SCONSEC, WCONSEC")
+        should_continue = input("Continue anyway? [Y/n]: ")
+        if should_continue.upper() == "N":
+            print("Exiting..")
+            return False
+             
 
     if args.structure in _valid_similar("subgraph"):
         print(generate_subgraphs(args.num_vertices,
-                           args.num_edges,
-                           args.duration,
-                           args.max_time,
-                           args.num_graphs))
+                                 args.num_edges,
+                                 args.duration,
+                                 args.max_time,
+                                 args.num_graphs, args.implicit))
         
     elif args.structure in _valid_similar("clique"):
         print("Disregarding edge information, clique size is determined by number of vertices")
         print(generate_cliques(args.num_vertices,
-                         args.duration,
-                         args.max_time,
-                         args.num_graphs))
+                               args.duration,
+                               args.max_time,
+                               args.num_graphs,
+                               args.implicit))
         
     elif args.structure in _valid_similar("tree"):
         print("Tree size is determined by number of vertices")
         print("Interpreting --num-edges/-e as max degree")
         print(generate_trees(args.num_vertices,
-                       args.duration,
-                       args.max_time,
-                       args.num_graphs))
+                             args.duration,
+                             args.max_time,
+                             args.num_graphs,
+                             args.implicit))
         
     elif args.structure in _valid_similar("path"):
         print("Disregarding edge information, path size is determined by number of vertices")
         print(generate_paths(args.num_vertices,
-                       args.duration,
-                       args.max_time,
-                       args.num_graphs))
+                             args.duration,
+                             args.max_time,
+                             args.num_graphs,
+                             args.implicit))
 
     else:
         print("Action ", args.structure, "not recognized" )
@@ -47,7 +62,7 @@ def main():
 
     return True
 
-def generate_subgraphs(v_num, e_num, max_dur, max_time, num_graphs):
+def generate_subgraphs(v_num, e_num, max_dur, max_time, num_graphs, implicit):
     graphs = []
     for i in range(num_graphs):
         edges = []        
@@ -55,7 +70,8 @@ def generate_subgraphs(v_num, e_num, max_dur, max_time, num_graphs):
             # the source and dest ids
             sid = random.randrange(v_num)
             did = random.randrange(v_num)
-            (start, end) = _random_time(max_dur, max_time)
+            (start, end) = _random_time(max_dur, max_time, implicit, edges, src,
+                                        dst)
             edges.append((eid,sid,did,start,end))
 
         graphs.append(edges)
@@ -69,7 +85,8 @@ def generate_cliques(v_num, max_dur, max_time, num_graphs):
         eid = 0
         for vid in range(vid):
             for uid in set(range(vid)) - set([vid]):
-                (start,end) = _random_time(max_dur, max_time)
+                (start,end) = _random_time(max_dur, max_time, implicit, edges,
+                                           src, dst)
                 edges.append(eid, vid, uid, start, end)
                 eid += 1
             
@@ -82,7 +99,8 @@ def generate_paths(v_num, max_dur, max_time, num_graphs):
         edges = []
         eid = 0
         for vid in range(v_num-1):
-            (start, end) = _random_time(max_dur, max_time)
+            (start, end) = _random_time(max_dur, max_time, implicit, edges, src,
+                                        dst)
             edges.append((eid, vid, vid+1, start, end))
             eid += 1
         graphs.append(edges)
@@ -98,7 +116,8 @@ def generate_trees(v_num, max_degree, max_dur, max_time, num_graphs):
             if fanout == 0:
                 fanout = random.randrange(max_degree)
                 for f in range(1,fanout):
-                    (start, end) = _random_times(max_dur, max_time)
+                    (start, end) = _random_times(max_dur, max_time, implicit,
+                                                 edges, src, dst)
                     edges.append((eid, vid, vid+f, start, end))
                     eid += 1
             else:
@@ -112,11 +131,53 @@ def generate_trees(v_num, max_degree, max_dur, max_time, num_graphs):
 def _valid_similar(name):
     return (name, name + "s", name.upper(), name.upper() + "S")
 
-def _random_time(max_dur, max_time):
+def _random_time(max_dur, max_time, implicit, edges, src, dst):
     dur = random.randrange(max_dur)
-    start = random.randrange(max_time - dur)
-    return (start, start+dur)
+    if implicit == "CONCUR":
+        intersection = _big_isect(edges)
+        
+        if intersection[0] > intersection[1]:
+            print("BAD INTERSECTION", intersection)
+            return _random_time(max_dur, max_time, "", edges, src, dst)
+        else:
+            start = random.randrange(intersection[0]+1-dur,intersection[1])
+            end = start + dur
 
+    elif implicit == "SCONSEC":
+        pred_isect = _big_isect(_get_pred_edges(edges,src), max_time)
+        succ_edges = _big_isect(_get_succ_edges(edges,dest), max_time)
+        t = random.randrange(*pred_isect)
+        s = random.randrange(*succ_edges)
+        start = min(t,s)
+        end = max(t,s)
+        
+    elif implicit == "WCONSEC":
+        pred_isect = _big_isect(_get_pred_edges(edges,src), max_time)
+        succ_edges = _big_isect(_get_succ_edges(edges,dest), max_time)
+        start = random.randrange(succ_edges[1])
+        end = random.randrange(max(start + dur, succ_edges[0]), max_time)
+    else:
+        start = random.randrange(max_time - dur)
+    return (start, end)
+
+def _get_pred_edges(edges, src):
+    return filter(lambda e: e[2] == src, edges)
+    
+    
+def _get_succ_edges(edges, dest):
+    return filter(lambda e: e[1] == dest, edges)
+
+def _big_isect(edges, max_time):
+    intersection = (-1, max_time)
+    for e in edges:
+        if intersection[0] == None:
+            intersection = (e[3], intersection[1])
+        if intersection[1] == None:
+            intersection = (intersection[0], e[4])
+
+        intersection = (max(intersection[0], e[3]),
+                        min(intersection[1], e[4]))
+    
 
 
 if __name__ == "__main__" : main()
